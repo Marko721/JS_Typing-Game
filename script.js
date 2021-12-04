7a88b4fc7b37906e160cf201e123c61142f9f7ff
unknown <popmark1002@gmail.com>
2021-12-05 00:42:00 +0100

    Adding the timer and game over modal

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// connect to a api to get random words
+// when you type a letter perform a check to the word green if it's correct red orange if it's not
+// display 3 rows of 5 words so you can see ahead for faster typing
+
 const word = document.getElementById("word");
 const text = document.getElementById("text");
 const scoreEl = document.getElementById("score");
 const timeEl = document.getElementById("time");
-const endgameEl = document.getElementById("end-game");
+const endgameEl = document.getElementById("end-game-container");
 const settingsBtn = document.getElementById("settings-btn");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
@@ -41,6 +45,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Focus on input on start
+text.focus();
+
+// Start counting down to run updateTime function every second
+const timeInterval = setInterval(updateTime, 1000);
+
 // Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -60,6 +70,30 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
+//Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + "s";
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+
+    // end game
+    gameOver();
+  }
+}
+
+// Game over
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = "flex";
+}
+
 // Event listeners
 
 text.addEventListener("input", (e) => {
@@ -71,6 +105,8 @@ text.addEventListener("input", (e) => {
 
     // Clear
     e.target.value = "";
+
+    time += 5;
   }
 });
 
